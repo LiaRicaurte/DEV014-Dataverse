5d5c80c0fb2b9acd8cbb47bda7e0bceb83358a19
LiaRicaurte <liaricaurte.arq@gmail.com>
2024-04-06 22:17:25 -0500

    comienzo de hito2 para visualizar en el archivo view

--- src/view.js
+++ src/view.js
@@ -1,6 +1,11 @@
-//export const renderItems = (data) => {
-//console.log(data)
+export const renderItems = (data) => {
+console.log(data)
 // Aquí comienza tu código y puedes retornar lo que tu necesites
-//return 'example';
-//};
+  const dataSet = document.createElement("ul");
+  data.foreach(card => {
+    const cardLi = document.createElement("li");
+    dataSet.appendChild(cardLi);
+  })
+  return 'example';
+};
 
