9a9dfac6b74a60e512f675f6b18dbeaffb8a793e
LiaRicaurte <liaricaurte.arq@gmail.com>
2024-04-10 09:06:47 -0500

    agrega metodo calsslist.add a los elementos del archivo view.js

--- src/view.js
+++ src/view.js
@@ -1,17 +1,26 @@
 export const renderItems = (data) => {
   console.log(data)
-  // Aquí comienza tu código y puedes retornar lo que tu necesites
+  // crear un elemento ul
   const ul = document.createElement("ul");
+  //agregar un metodo que permite poner estilos al elemento ul
+  ul.classList.add('container');
+  //
   data.forEach(card => {
     const cardLi = document.createElement("li");
     const p = document.createElement("p");
     const img = document.createElement("img");
     const h4 = document.createElement("h4");
+    
     cardLi.textContent = card.name;
     p.textContent = card.shortDescription;
     img.src = card.imagePath;
     h4.textContent = card.facts.releaseYear;
     ul.append(cardLi, img, h4, p);
+
+    cardLi.classList.add('card');
+    p.classList.add('p');
+    img.classList.add('img');
+    h4.classList.add('h4');
   })
   return ul;
 };
