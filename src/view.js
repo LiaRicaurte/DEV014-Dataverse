97cd98b25fb3f687321166cccda0f150cc0e8daf
LiaRicaurte <liaricaurte.arq@gmail.com>
2024-04-15 15:16:52 -0500

    cambia el parametro card por el parametro movie e incorpora a manera de comettarios el metodo semanticHTML

--- src/view.js
+++ src/view.js
@@ -5,29 +5,42 @@ export const renderItems = (data) => {
   //agregar un metodo que permite poner estilos al elemento ul
   ul.classList.add('flex-container');
   //
-  data.forEach(card => {
-    const cardLi = document.createElement("li");
-    const cardImg = document.createElement ("img")
-    const cardDiv = document.createElement ("div")
+  data.forEach(movie => {
+    const movieLi = document.createElement("li");
+    //const semanticHTML= `
+    //<dl itemscope itemtype="WomenInTech">
+    //<img src="URL_DE_LA_IMAGEN_GENERADA" alt="Ada Lovelace" />
+    //<dt>Nombre:</dt><dd itemprop="name">${movie.name}</dd>
+    //<dt>Descripción:</dt><dd itemprop="description">Pionera de la informática, fue la primera programadora.</dd>
+    //<dt>Año de nacimiento:</dt><dd itemprop="yearOfBirth">1843</dd>
+    //<dt>Lugar de nacimiento:</dt><dd itemprop="placeOfBirth">London, England</dd>
+    //<dt>Campo de desempeño:</dt><dd itemprop="mainField">Computer Science</dd>
+    //</dl>  
+    // `
+    const movieImg = document.createElement ("img")
+    const movieDiv = document.createElement ("div")
     const h3 = document.createElement("h3")
     const p = document.createElement("p");
     const h4 = document.createElement("h4");
     
-    cardImg.src = card.imagePath;
-    h3.textContent = card.name
-    p.textContent = card.shortDescription;
-    h4.textContent = card.facts.releaseYear;
-    
-    ul.append(cardLi);
-    cardLi.append(cardImg, cardDiv);
-    cardDiv.append(h3, p, h4)
+    movieImg.src = movie.imagePath;
+    h3.textContent = movie.name
+    p.textContent = movie.shortDescription;
+    h4.textContent = movie.facts.releaseYear;
 
-    cardLi.classList.add('flex-item');
-    cardImg.classList.add('img');
-    cardDiv.classList.add('div')
+    //movieLi.innerHTML=semanticHTML;
+
+    ul.append(movieLi);
+    movieLi.append(movieImg, movieDiv);
+    movieDiv.append(h3, p, h4)
+
+    movieLi.classList.add('flex-item');
+    movieImg.classList.add('img');
+    movieDiv.classList.add('div')
     h3.classList.add('h3');
     p.classList.add('p');
     h4.classList.add('h4');
+
   })
   return ul;
 };
