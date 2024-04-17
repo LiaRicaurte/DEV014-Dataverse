283eb58b3cbf4f28e2eecdda32f0231012890d55
LiaRicaurte <liaricaurte.arq@gmail.com>
2024-04-16 21:23:26 -0500

    aplica estilos a las tarjetas a traves de los atributos itemprop

--- src/view.js
+++ src/view.js
@@ -8,14 +8,11 @@ export const renderItems = (data) => {
   data.forEach(movie => {
     const movieLi = document.createElement("li");
     const semanticHTML= `
-    <dl itemscope itemtype="ClásicosdelCine">
+    <dl itemscope itemtype="ClassicMovies">
     <img src="${movie.imagePath}" alt="${movie.name}" />
-    <dt>Nombre:</dt><dd itemprop="name">${movie.name}</dd>
-    <dt>Género:</dt><dd itemprop="shortDescription">${movie.shortDescription}</dd>
-    <dt>Descripción:</dt><dd itemprop="description">${movie.description}</dd>
-    <dt>Fecha de Estreno:</dt><dd itemprop="releaseYear">${movie.facts.releaseYear}</dd>
-    <dt>Tiempo de duración:</dt><dd itemprop="durationTime">${movie.facts.durationTime}</dd>
-    <dt>Clasificación:</dt><dd itemprop="classification">${movie.facts.classification}</dd>
+    <dd itemprop="name">${movie.name}</dd>
+    <dd itemprop="shortDescription">${movie.shortDescription}</dd>
+    <dd itemprop="releaseYear">${movie.facts.releaseYear}</dd>
     </dl>  
      `
 
